8f373f08f3049eb52bfcd938722f566136d9ad74
rofinn <rory.finnegan@gmail.com>
2021-03-09 11:35:11 -0600

    Restrict doctests to 1.5 or higher due to REPL output changes.

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,5 +11,6 @@ using AxisSets: Dataset, Pattern, constraintmap, dimpaths, flatten, validate
     include("flatten.jl")
     include("patterns.jl")
     include("dataset.jl")
-    doctest(AxisSets)
+    # Repl output changes across julia versions and architectures
+    Sys.WORD_SIZE == 64 && VERSION >= v"1.5" && doctest(AxisSets)
 end
