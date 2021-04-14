604453fcd6b3703e0cf313ae67d862b8053591c3
Ben Cottier <ben.cottier@invenialabs.co.uk>
2021-04-14 12:07:54 +0100

    Update tests for all apply methods

--- test/featuretransforms.jl
+++ test/featuretransforms.jl
@@ -1,13 +1,17 @@
 @testset "FeatureTransforms" begin
+    M1 = [0.0 1.0; 1.0 2.0; -0.5 0.0]
+    M2 = [-2.0 4.0; 3.0 2.0; -1.0 -1.0]
+    M3 = [0.0 1.0; -1.0 0.5; -0.5 0.0]
+    M4 = [0.5 -1.0; -5.0 -2.0; 0.0 1.0]
     ds = KeyedDataset(
         flatten([
             :train => [
-                :load => KeyedArray([7.0 7.7; 8.0 8.2; 9.0 9.9]; time=1:3, loc=[:x, :y]),
-                :price => KeyedArray([-2.0 4.0; 3.0 2.0; -1.0 -1.0]; time=1:3, id=[:a, :b]),
+                :load => KeyedArray(M1; time=1:3, loc=[:x, :y]),
+                :price => KeyedArray(M2; time=1:3, id=[:a, :b]),
             ],
             :predict => [
-                :load => KeyedArray([7.0 7.7; 8.1 7.9; 9.0 9.9]; time=1:3, loc=[:x, :y]),
-                :price => KeyedArray([0.5 -1.0; -5.0 -2.0; 0.0 1.0]; time=1:3, id=[:a, :b]),
+                :load => KeyedArray(M3; time=1:3, loc=[:x, :y]),
+                :price => KeyedArray(M4; time=1:3, id=[:a, :b]),
             ]
         ])...
     )
@@ -16,39 +20,107 @@
         @test is_transformable(ds)
     end
 
-    @testset "apply" begin
+    # TODO: use fake Transforms
+    @testset "apply OneToOne" begin
         p = Power(2)
 
-        expected = KeyedDataset(
-            flatten([
-                :train => [
-                    :price => KeyedArray([4.0 16.0; 9.0 4.0; 1.0 1.0]; time=1:3, id=[:a, :b]),
-                ],
-                :predict => [
-                    :price => KeyedArray([0.25 1.0; 25.0 4.0; 0.0 1.0]; time=1:3, id=[:a, :b]),
-                ]
-            ])...
-        )
+        @testset "apply" begin
+            @testset "one feature" begin
+                expected = KeyedDataset(
+                    flatten([
+                        :train => [
+                            :price => KeyedArray(M2.^2; time=1:3, id=[:a, :b]),
+                        ],
+                        :predict => [
+                            :price => KeyedArray(M4.^2; time=1:3, id=[:a, :b]),
+                        ]
+                    ])...
+                )
 
-        r = FeatureTransforms.apply(ds, p; dims=(:_, :price, :_))
+                r = FeatureTransforms.apply(ds, p; dims=(:_, :price, :_))
 
-        @test r isa KeyedDataset
-        @test isequal(r, expected)
+                @test r isa KeyedDataset
+                @test isequal(r, expected)
+            end
 
-        expected = KeyedDataset(
-            flatten([
-                :train => [
-                    :price => KeyedArray(hcat([16.0; 4.0; 1.0]); time=1:3, id=[:b]),
-                ],
-                :predict => [
-                    :price => KeyedArray(hcat([1.0; 4.0; 1.0]); time=1:3, id=[:b]),
-                ]
-            ])...
-        )
+            @testset "all features" begin
+                expected = KeyedDataset(
+                    flatten([
+                        :train => [
+                            :load => KeyedArray(M1.^2; time=1:3, loc=[:x, :y]),
+                            :price => KeyedArray(M2.^2; time=1:3, id=[:a, :b]),
+                        ],
+                        :predict => [
+                            :load => KeyedArray(M3.^2; time=1:3, loc=[:x, :y]),
+                            :price => KeyedArray(M4.^2; time=1:3, id=[:a, :b]),
+                        ]
+                    ])...
+                )
 
-        r = FeatureTransforms.apply(ds, p; dims=(:_, :price, :id), inds=[2])
+                r = FeatureTransforms.apply(ds, p; dims=:)
 
-        @test r isa KeyedDataset
-        @test isequal(r, expected)
+                @test r isa KeyedDataset
+                @test isequal(r, expected)
+            end
+
+            @testset "inds" begin
+                expected = KeyedDataset(
+                    flatten([
+                        :train => [
+                            :price => KeyedArray(hcat((M2.^2)[:, 2]); time=1:3, id=[:b]),
+                        ],
+                        :predict => [
+                            :price => KeyedArray(hcat((M4.^2)[:, 2]); time=1:3, id=[:b]),
+                        ]
+                    ])...
+                )
+
+                r = FeatureTransforms.apply(ds, p; dims=(:_, :price, :id), inds=[2])
+
+                @test r isa KeyedDataset
+                @test isequal(r, expected)
+            end
+        end
+
+        @testset "apply!" begin
+            expected = KeyedDataset(
+                flatten([
+                    :train => [
+                        :load => KeyedArray(M1; time=1:3, loc=[:x, :y]),
+                        :price => KeyedArray(M2.^2; time=1:3, id=[:a, :b]),
+                    ],
+                    :predict => [
+                        :load => KeyedArray(M3; time=1:3, loc=[:x, :y]),
+                        :price => KeyedArray(M4.^2; time=1:3, id=[:a, :b]),
+                    ]
+                ])...
+            )
+
+            r = FeatureTransforms.apply!(ds, p; dims=(:_, :price, :_))
+
+            @test isequal(ds, expected)
+            @test r isa KeyedDataset
+            @test isequal(r, expected)
+        end
+
+        @testset "apply_append" begin
+            M2_cat = cat(M2, M2.^2, dims=2)
+            M4_cat = cat(M4, M4.^2, dims=2)
+            expected = KeyedDataset(
+                flatten([
+                    :train => [
+                        :price => KeyedArray(M2_cat; time=1:3, id=[:a, :b, :a, :b]),
+                    ],
+                    :predict => [
+                        :price => KeyedArray(M4_cat; time=1:3, id=[:a, :b, :a, :b]),
+                    ]
+                ])...
+            )
+
+            r = FeatureTransforms.apply_append(ds, p; dims=(:_, :price, :_), append_dim=2)
+
+            @test r isa KeyedDataset
+            @test isequal(r, expected)
+        end
     end
 end
