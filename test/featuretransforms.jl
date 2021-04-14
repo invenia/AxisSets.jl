74f68591301c554a8eb74be1ed25408f396cb032
Ben Cottier <ben.cottier@invenialabs.co.uk>
2021-04-14 18:41:46 +0100

    Make apply methods return full dataset and use map
    - Map simplifies things - don't need the custom _apply_paths() anymore
    - Returning full dataset seems more appropriate after talking to Rory

--- test/featuretransforms.jl
+++ test/featuretransforms.jl
@@ -29,18 +29,21 @@
                 expected = KeyedDataset(
                     flatten([
                         :train => [
+                            :load => KeyedArray(M1; time=1:3, loc=[:x, :y]),
                             :price => KeyedArray(M2.^2; time=1:3, id=[:a, :b]),
                         ],
                         :predict => [
+                            :load => KeyedArray(M3; time=1:3, loc=[:x, :y]),
                             :price => KeyedArray(M4.^2; time=1:3, id=[:a, :b]),
                         ]
                     ])...
                 )
 
-                r = FeatureTransforms.apply(ds, p; dims=(:_, :price, :_))
+                r = FeatureTransforms.apply(ds, p, (:_, :price, :_))
 
                 @test r isa KeyedDataset
                 @test isequal(r, expected)
+                @test !isequal(ds, expected)
             end
 
             @testset "all features" begin
@@ -61,24 +64,28 @@
 
                 @test r isa KeyedDataset
                 @test isequal(r, expected)
+                @test !isequal(ds, expected)
             end
 
             @testset "inds" begin
                 expected = KeyedDataset(
                     flatten([
                         :train => [
+                            :load => KeyedArray(M1; time=1:3, loc=[:x, :y]),
                             :price => KeyedArray(hcat((M2.^2)[:, 2]); time=1:3, id=[:b]),
                         ],
                         :predict => [
+                            :load => KeyedArray(M3; time=1:3, loc=[:x, :y]),
                             :price => KeyedArray(hcat((M4.^2)[:, 2]); time=1:3, id=[:b]),
                         ]
                     ])...
                 )
 
-                r = FeatureTransforms.apply(ds, p; dims=(:_, :price, :id), inds=[2])
+                r = FeatureTransforms.apply(ds, p, (:_, :price, :_); dims=:id, inds=[2])
 
                 @test r isa KeyedDataset
                 @test isequal(r, expected)
+                @test !isequal(ds, expected)
             end
         end
 
@@ -96,31 +103,38 @@
                 ])...
             )
 
-            r = FeatureTransforms.apply!(ds, p; dims=(:_, :price, :_))
+            r = FeatureTransforms.apply!(ds, p, (:_, :price, :_))
 
-            @test isequal(ds, expected)
             @test r isa KeyedDataset
             @test isequal(r, expected)
+            @test isequal(ds, expected)
         end
 
         @testset "apply_append" begin
-            M2_cat = cat(M2, M2.^2, dims=2)
-            M4_cat = cat(M4, M4.^2, dims=2)
-            expected = KeyedDataset(
-                flatten([
-                    :train => [
-                        :price => KeyedArray(M2_cat; time=1:3, id=[:a, :b, :a, :b]),
-                    ],
-                    :predict => [
-                        :price => KeyedArray(M4_cat; time=1:3, id=[:a, :b, :a, :b]),
-                    ]
-                ])...
-            )
+            @testset "inner" begin
+                M2_cat = cat(M2, M2.^2, dims=2)
+                M4_cat = cat(M4, M4.^2, dims=2)
+                expected = KeyedDataset(
+                    flatten([
+                        :train => [
+                            :load => KeyedArray(M1; time=1:3, loc=[:x, :y]),
+                            :price => KeyedArray(M2_cat; time=1:3, id=[:a, :b, :a, :b]),
+                        ],
+                        :predict => [
+                            :load => KeyedArray(M3; time=1:3, loc=[:x, :y]),
+                            :price => KeyedArray(M4_cat; time=1:3, id=[:a, :b, :a, :b]),
+                        ]
+                    ])...
+                )
 
-            r = FeatureTransforms.apply_append(ds, p; dims=(:_, :price, :_), append_dim=2)
+                r = FeatureTransforms.apply_append(
+                    ds, p, (:_, :price, :_);
+                    inner=true, append_dim=2
+                )
 
-            @test r isa KeyedDataset
-            @test isequal(r, expected)
+                @test r isa KeyedDataset
+                @test isequal(r, expected)
+            end
         end
     end
 end
