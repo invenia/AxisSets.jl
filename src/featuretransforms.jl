48bf05156fa2d197a6390bd8b5ffb4c3eaa043e3
Ben Cottier <ben.cottier@invenialabs.co.uk>
2021-04-15 19:03:23 +0100

    Update docstrings and doctests

--- src/featuretransforms.jl
+++ src/featuretransforms.jl
@@ -8,10 +8,14 @@ _transform_pattern(dims) = Pattern[(:__, d) for d in dims]
 """
     FeatureTransforms.apply(ds::KeyedDataset, t::Transform, [key]; dims=:, kwargs...)
 
-Apply the `Transform` to components of the [`KeyedDataset`](@ref) along dimension `dims`.
-The transform can be applied to a subselection of components via a [`Pattern`](@ref) `key`.
+Apply the `Transform` to each component of the [`KeyedDataset`](@ref).
+Returns a new dataset with the same constraints, but transformed components.
 
-Keyword arguments are passed to the equivalent `FeatureTransforms` method for a component.
+The transform can be applied to a subselection of components via a [`Pattern`](@ref) `key`.
+Otherwise, components are selected by the desired `dims`.
+
+Keyword arguments including `dims` are passed to the appropriate `FeatureTransforms` method
+for a component.
 
 # Example
 ```jldoctest
@@ -32,11 +36,13 @@ julia> ds = KeyedDataset(
 
 julia> p = Power(2);
 
-julia> r = FeatureTransforms.apply(ds, p; dims=(:_, :price, :_));
+julia> r = FeatureTransforms.apply(ds, p, (:_, :price, :_));
 
 julia> [k => parent(parent(v)) for (k, v) in r.data]
-2-element Vector{Pair{Tuple{Symbol, Symbol}, Matrix{Float64}}}:
+4-element Vector{Pair{Tuple{Symbol, Symbol}, Matrix{Float64}}}:
+    (:train, :load) => [7.0 7.7; 8.0 8.2; 9.0 9.9]
    (:train, :price) => [4.0 16.0; 9.0 4.0; 1.0 1.0]
+  (:predict, :load) => [7.0 7.7; 8.1 7.9; 9.0 9.9]
  (:predict, :price) => [0.25 1.0; 25.0 4.0; 0.0 1.0]
 ```
 """
@@ -52,11 +58,14 @@ end
 """
     FeatureTransforms.apply!(ds::KeyedDataset, t::Transform, [key]; dims=:, kwargs...)
 
-Apply the `Transform` to components of the [`KeyedDataset`](@ref) along dimension `dims`,
-mutating the components in-place.
-The transform can be applied to a subselection of components via a [`Pattern`](@ref) `key`.
+Apply the `Transform` to each component of the [`KeyedDataset`](@ref).
+Returns a new dataset with the same constraints, but transformed components.
 
-Keyword arguments are passed to the equivalent `FeatureTransforms` method for a component.
+The transform can be applied to a subselection of components via a [`Pattern`](@ref) `key`.
+Otherwise, components are selected by the desired `dims`.
+
+Keyword arguments including `dims` are passed to the appropriate `FeatureTransforms` method
+for a component.
 """
 function FeatureTransforms.apply!(
     ds::KeyedDataset, t::Transform, keys...;
@@ -68,12 +77,55 @@ function FeatureTransforms.apply!(
 end
 
 """
-    FeatureTransforms.apply_append(ds::KeyedDataset, t::Transform, [key]; dims=:, kwargs...)
+    FeatureTransforms.apply_append(
+        ds::KeyedDataset, t::Transform, [key];
+        dims=:, inner=false, component_name=:component, kwargs...
+    )
 
-Apply the `Transform` to components of the [`KeyedDataset`](@ref) along dimension `dims`.
+Apply the `Transform` to each component of the [`KeyedDataset`](@ref).
+
 The transform can be applied to a subselection of components via a [`Pattern`](@ref) `key`.
+Otherwise, components are selected by the desired `dims`.
 
-Keyword arguments are passed to the equivalent `FeatureTransforms` method for a component.
+If `inner=true`, perform `FeatureTransforms.apply_append` on each component,
+returning a new dataset with the same constraints, but transformed components.
+
+Otherwise, transform each component using `FeatureTransforms.apply`, and append
+to a copy of the dataset as a new component called `component_name`.
+
+Keyword arguments including `dims` are passed to the appropriate `FeatureTransforms` method
+for a component.
+
+# Example
+```jldoctest
+julia> using AxisKeys, FeatureTransforms; using AxisSets: KeyedDataset, Pattern, flatten;
+
+julia> ds = KeyedDataset(
+           flatten([
+               :train => [
+                   :load => KeyedArray([7.0 7.7; 8.0 8.2; 9.0 9.9]; time=1:3, loc=[:x, :y]),
+                   :price => KeyedArray([-2.0 4.0; 3.0 2.0; -1.0 -1.0]; time=1:3, id=[:a, :b]),
+               ],
+               :predict => [
+                   :load => KeyedArray([7.0 7.7; 8.1 7.9; 9.0 9.9]; time=1:3, loc=[:x, :y]),
+                   :price => KeyedArray([0.5 -1.0; -5.0 -2.0; 0.0 1.0]; time=1:3, id=[:a, :b]),
+               ]
+           ])...
+       );
+
+julia> p = Power(2);
+
+julia> r = FeatureTransforms.apply_append(ds, p, (:_, :price, :_); component_name=:price2);
+
+julia> [k => parent(parent(v)) for (k, v) in r.data]
+6-element Vector{Pair{Tuple{Symbol, Symbol}, Matrix{Float64}}}:
+     (:train, :load) => [7.0 7.7; 8.0 8.2; 9.0 9.9]
+    (:train, :price) => [-2.0 4.0; 3.0 2.0; -1.0 -1.0]
+   (:predict, :load) => [7.0 7.7; 8.1 7.9; 9.0 9.9]
+  (:predict, :price) => [0.5 -1.0; -5.0 -2.0; 0.0 1.0]
+   (:train, :price2) => [4.0 16.0; 9.0 4.0; 1.0 1.0]
+ (:predict, :price2) => [0.25 1.0; 25.0 4.0; 0.0 1.0]
+```
 """
 function FeatureTransforms.apply_append(
     ds::KeyedDataset, t::Transform, keys...;
