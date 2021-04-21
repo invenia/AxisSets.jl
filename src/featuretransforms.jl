ec8f6e0c521f0a019c551f32c7731d7028c4828a
Glenn Moynihan <glenn.moynihan@invenialabs.co.uk>
2021-04-21 17:16:31 +0100

    Remove apply! and apply_append methods

--- src/featuretransforms.jl
+++ src/featuretransforms.jl
@@ -1,10 +1,3 @@
-FeatureTransforms.is_transformable(::KeyedDataset) = true
-
-_transform_pattern(keys, dims) = isempty(keys) ? _transform_pattern(dims) : Pattern[keys...]
-_transform_pattern(::Colon) = Pattern[(:__,)]
-_transform_pattern(dims::Symbol) = Pattern[(:__, dims)]
-_transform_pattern(dims) = Pattern[(:__, d) for d in dims]
-
 """
     FeatureTransforms.apply(ds::KeyedDataset, t::Transform, [key]; dims=:, kwargs...)
 
@@ -46,109 +39,13 @@ julia> [k => parent(parent(v)) for (k, v) in r.data]
  (:predict, :price) => [0.25 1.0; 25.0 4.0; 0.0 1.0]
 ```
 """
-function FeatureTransforms.apply(
-    ds::KeyedDataset, t::Transform, keys...;
-    dims=:, kwargs...
-)
+function FeatureTransforms.apply(ds::KeyedDataset, t::Transform, keys...; dims=:, kwargs...)
     return map(ds, _transform_pattern(keys, dims)...) do a
         FeatureTransforms.apply(a, t; dims=dims, kwargs...)
     end
 end
 
-"""
-    FeatureTransforms.apply!(ds::KeyedDataset, t::Transform, [key]; dims=:, kwargs...)
-
-Apply the `Transform` to each component of the [`KeyedDataset`](@ref).
-Returns a new dataset with the same constraints, but transformed components.
-
-The transform can be applied to a subselection of components via a [`Pattern`](@ref) `key`.
-Otherwise, components are selected by the desired `dims`.
-
-Keyword arguments including `dims` are passed to the appropriate `FeatureTransforms` method
-for a component.
-"""
-function FeatureTransforms.apply!(
-    ds::KeyedDataset, t::Transform, keys...;
-    dims=:, kwargs...
-)
-    return map(ds, _transform_pattern(keys, dims)...) do a
-        FeatureTransforms.apply!(a, t; dims=dims, kwargs...)
-    end
-end
-
-"""
-    FeatureTransforms.apply_append(
-        ds::KeyedDataset, t::Transform, [key];
-        dims=:, inner=false, component_name=:component, kwargs...
-    )
-
-Apply the `Transform` to each component of the [`KeyedDataset`](@ref).
-
-The transform can be applied to a subselection of components via a [`Pattern`](@ref) `key`.
-Otherwise, components are selected by the desired `dims`.
-
-If `inner=true`, perform `FeatureTransforms.apply_append` on each component,
-returning a new dataset with the same constraints, but transformed components.
-
-Otherwise, transform each component using `FeatureTransforms.apply`, and append
-to a copy of the dataset as a new component called `component_name`.
-
-Keyword arguments including `dims` are passed to the appropriate `FeatureTransforms` method
-for a component.
-
-# Example
-```jldoctest
-julia> using AxisKeys, FeatureTransforms; using AxisSets: KeyedDataset, Pattern, flatten;
-
-julia> ds = KeyedDataset(
-           flatten([
-               :train => [
-                   :load => KeyedArray([7.0 7.7; 8.0 8.2; 9.0 9.9]; time=1:3, loc=[:x, :y]),
-                   :price => KeyedArray([-2.0 4.0; 3.0 2.0; -1.0 -1.0]; time=1:3, id=[:a, :b]),
-               ],
-               :predict => [
-                   :load => KeyedArray([7.0 7.7; 8.1 7.9; 9.0 9.9]; time=1:3, loc=[:x, :y]),
-                   :price => KeyedArray([0.5 -1.0; -5.0 -2.0; 0.0 1.0]; time=1:3, id=[:a, :b]),
-               ]
-           ])...
-       );
-
-julia> p = Power(2);
-
-julia> r = FeatureTransforms.apply_append(ds, p, (:_, :price, :_); component_name=:price2);
-
-julia> [k => parent(parent(v)) for (k, v) in r.data]
-6-element Vector{Pair{Tuple{Symbol, Symbol}, Matrix{Float64}}}:
-     (:train, :load) => [7.0 7.7; 8.0 8.2; 9.0 9.9]
-    (:train, :price) => [-2.0 4.0; 3.0 2.0; -1.0 -1.0]
-   (:predict, :load) => [7.0 7.7; 8.1 7.9; 9.0 9.9]
-  (:predict, :price) => [0.5 -1.0; -5.0 -2.0; 0.0 1.0]
-   (:train, :price2) => [4.0 16.0; 9.0 4.0; 1.0 1.0]
- (:predict, :price2) => [0.25 1.0; 25.0 4.0; 0.0 1.0]
-```
-"""
-function FeatureTransforms.apply_append(
-    ds::KeyedDataset, t::Transform, keys...;
-    dims=:, inner=false, component_name=:component, kwargs...
-)
-    patterns = _transform_pattern(keys, dims)
-
-    if inner  # batched apply_append on each component
-        return map(ds, patterns...) do a
-            FeatureTransforms.apply_append(a, t; dims=dims, kwargs...)
-        end
-    else  # merge transformed components as new components of dataset
-        # select any components the keys match
-        selected = unique(x[1:end-1] for x in dimpaths(ds) if any(p -> x in p, patterns))
-
-        # construct keys of new transformed components
-        new_keys = [(k[1:end-1]..., component_name) for k in selected]
-
-        # pair new keys with transformed components
-        pairs = map(new_keys, selected) do new_k, k
-            new_k => FeatureTransforms.apply(ds.data[k], t; dims=dims, kwargs...)
-        end
-
-        return merge(ds, KeyedDataset(pairs...))
-    end
-end
+_transform_pattern(keys, dims) = isempty(keys) ? _transform_pattern(dims) : Pattern[keys...]
+_transform_pattern(::Colon) = Pattern[(:__,)]
+_transform_pattern(dims::Symbol) = Pattern[(:__, dims)]
+_transform_pattern(dims) = Pattern[(:__, d) for d in dims]
